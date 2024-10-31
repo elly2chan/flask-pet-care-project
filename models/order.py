23b20703951f547e02c3251e570a6a8b5944b5a1
elly2chan <elena.iv.konstantinova@gmail.com>
2024-10-31 21:43:56 +0200

    Refactor models and do initial migration

--- models/order.py
+++ models/order.py
@@ -4,9 +4,6 @@ from sqlalchemy import func
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 from db import db
-from models.enums import State
-from models.product import ProductModel
-from models.user import UserModel
 
 
 class OrderModel(db.Model):
@@ -15,14 +12,9 @@ class OrderModel(db.Model):
     id: Mapped[int] = mapped_column(primary_key=True)
     created_on: Mapped[datetime] = mapped_column(db.DateTime, server_default=func.now())
     address: Mapped[str] = mapped_column(db.String(255), nullable=False)
-    customer_id: Mapped[int] = mapped_column(db.Integer, db.ForeignKey("users.id"))
-    customer: Mapped["UserModel"] = relationship("UserModel")
-    product_id: Mapped[int] = mapped_column(db.Integer, db.ForeignKey("products.id"))
-    product_title: Mapped[str] = mapped_column(db.String(100), db.ForeignKey("products.title"))
-    product_description: Mapped[str] = mapped_column(db.Text, db.ForeignKey("products.description"))
-    product: Mapped["ProductModel"] = relationship("ProductModel")
-    status: Mapped[State] = mapped_column(
-        db.Enum(State),
-        nullable=False,
-        default=State.pending.name
-    )
+    customer_id: Mapped[int] = mapped_column(db.Integer, nullable=False)
+    product_id: Mapped[int] = mapped_column(db.Integer, nullable=False)
+    status: Mapped[str] = mapped_column(db.Enum('pending', 'approved', 'rejected', name='state'), nullable=False)
+
+    customer = db.relationship('UserModel', backref='orders')
+    product = db.relationship('ProductModel', backref='orders')
